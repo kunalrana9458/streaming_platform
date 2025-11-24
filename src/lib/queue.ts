fecf381ac6c1cea5fdb53fac7e9ad36929805b4c
kunalrana9458 <kunalranabsr9458@gmail.com>
2025-11-24 17:49:17 +0530

    feat(billing): Queue setup for the webhook process by bullMq worker

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -16,8 +16,23 @@ export const connection = REDIS_URL
 
 export const MEDIA_QUEUE_NAME = 'media-processing';
 
+// define the queue for the webhook job processing
+export const WEBHOOK_QUEUE_NAME = 'stripe:webhook:queue';
+
 // Producer queue (re-use the same options)
 export const mediaQueue = new Queue(MEDIA_QUEUE_NAME, {
   connection
 });
 
+export const webhookQueue = new Queue(WEBHOOK_QUEUE_NAME,{
+  connection,
+  defaultJobOptions: {
+    attempts: 5,
+    backoff: {
+      type: 'exponential',
+      delay: 1000
+    },
+    removeOnComplete: true,
+    removeOnFail: false
+  }
+})
